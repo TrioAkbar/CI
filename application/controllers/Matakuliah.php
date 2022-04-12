e7c7062edf9ffef79d6dcfe36b41db8669e319fb
TrioAkbar <trioakbar162@gmail.com>
2022-04-12 16:16:16 +0700

    pertemuan 5

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -9,12 +9,24 @@
     
     public function cetak()
     {
-        $data = [
-            'kode' => $this->input->post('kode'),
-            'nama' => $this->input->post('nama'),
-            'sks' => $this->input->post('sks')
+            $this->form_validation->set_rules('kode','kode Matakuliah','required|min_length[3]',[
+                'required' =>'Kode Matakuliah Harus Diisi',
+                'min_lenght'=> 'Kode terlalu pendek'
+            ]);
+            $this->form_validation->set_rules('kode','kode Matakuliah','required|min_length[3]',[
+                'required' =>'Kode Matakuliah Harus Diisi',
+                'min_lenght'=> 'Kode terlalu pendek'
+            ]);
+            if($this->form_validation->run() != true){
+                $this->load->view('view-form-matakuliah');
+            } else{
+                $data = [
+                    'kode' => $this->input->post('kode'),
+                    'nama' => $this->input->post('nama'),
+                    'sks' => $this->input->post('sks')
         ];
         
-        $this->load->view('view-data-matakuliah', $data);
-     }
+            $this->load->view('view-data-matakuliah', $data);
+         }
+    }
 }
